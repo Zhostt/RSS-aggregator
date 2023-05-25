1eeb5724a41eae73224707d97ac55a30b8e0d90b
Zhostt <crazyplumber@yandex.ru>
2023-05-25 20:21:15 +0300

    view.js - added clearFeedback function, added it correctly to view onWatch actions

--- src/js/view.js
+++ src/js/view.js
@@ -1,20 +1,15 @@
 import onChange from 'on-change';
 
 const renderStateOnWatch = (state, elements, i18nextInstance) => {
+  const clearFeedback = () => document.querySelectorAll('.feedback').forEach((el) => el.remove()); // remove prev feedback messages
   const watchedFeedForm = onChange(state, (path, value, previousValue) => {
     // if submit is valid - delete red frame
     if (path === 'urlForm.valid' && value === true) {
-      document.querySelectorAll('.feedback').forEach((el) => el.remove()); // remove prev feedback messages
       elements.formInput.classList.remove('is-invalid');
-      // feedback that feed loaded properly
-      const successP = document.createElement('p');
-      successP.classList.add('feedback', 'm-0', 'position-relative', 'small', 'text-success');
-      successP.textContent = i18nextInstance.t('validation.success');
-      elements.form.appendChild(successP);
-
+      clearFeedback();
       // if submit is invalid (newly invalid or was invalid and invalid again)
     } else if ((path === 'urlForm.valid' && value === false) || (path === 'urlForm.errors' && value !== [])) {
-      document.querySelectorAll('.feedback').forEach((el) => el.remove()); // remove prev feedback messages
+      clearFeedback();
       elements.formInput.classList.add('is-invalid'); // making input invalid
       state.urlForm.errors.forEach((err) => { // create p elem for each error and add error text
         const errorP = document.createElement('p');
@@ -30,6 +25,12 @@ const renderStateOnWatch = (state, elements, i18nextInstance) => {
       const lastAddedFeed = value[value.length - 1]; // select last added feed, our value is renewed array of objects (state.feeds)
       feedLi.innerHTML = `<h3 class="channel-title h6 m-0">${lastAddedFeed.title}</h2><p class="channel-descr">${lastAddedFeed.description}</p>`;
       elements.feeds.append(feedLi); // insert created el as first one
+      // feedback that feed loaded properly
+      clearFeedback();
+      const successP = document.createElement('p');
+      successP.classList.add('feedback', 'm-0', 'position-relative', 'small', 'text-success');
+      successP.textContent = i18nextInstance.t('validation.success');
+      elements.form.appendChild(successP);
     }
     // add any new posts
     if (path === 'posts') {
